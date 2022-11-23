f349bd87be680c11a22e6d1bf1fe43054d4e790d
Barreto Vargas <97910210+Vargas33@users.noreply.github.com>
2022-11-23 10:57:46 -0300

    Update instance.tf

--- terraform-hello/instance.tf
+++ terraform-hello/instance.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "cloudbbuildterraform" {
   project      = "nbkub-363620"
   name         = "cloudbbuildterraform"
   machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  zone         = "us-west1-c"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
